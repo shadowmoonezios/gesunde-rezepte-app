c00cd53a30ff9cddb05aadc535e52c36119eff14
shadowmoonezios <agushifelde@hotmail.com>
2025-11-08 15:16:06 +0200

    feat: initial commit for client/src/components/RecipeList.js

---
+++ client/src/components/RecipeList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RecipeList = () => {
+  return (
+    <div>
+      <h2>Rezeptliste</h2>
+      {/* Hier kommt Ihre Rezeptliste */}
+    </div>
+  );
+};
+
+export default RecipeList;
