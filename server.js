ae2dc56a48f75bc00bb88d428b664749fb848627
shadowmoonezios <agushifelde@hotmail.com>
2025-11-08 15:18:18 +0200

    Refactor database connection URI for better readability.

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/gesunde-rezepte';
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Healthy Recipes App!');
 });
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/gesunde-rezepte', {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
